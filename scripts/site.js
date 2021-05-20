89fd215847aa767e63e2f9b4931857c8b74c71b9
Flor <florwalravens@hotmail.com>
2021-05-20 12:59:27 +0200

    Contact page finished

--- scripts/site.js
+++ scripts/site.js
@@ -1,11 +1,14 @@
-function init()
+function sendMail()
 {
-   
-    const img=document.getElementById("indexBG");
-    img.onclick=()=>{
-        img.style.animation=""
-        img.style.display="none";
-    }
+    const name=document.getElementById("Naam").value;
+    const subject=document.getElementById("Subject").value;
+    const email=document.getElementById("Email").value;
+    const message=document.getElementById("Message").value;
+
+    const body=`Naam: ${name}
+                E-mail: ${email} 
+                Bericht: ${message}`;
     
+    window.open(`mailto:florwalravens@hotmail.com?subject=${subject}&body=${body}`);
+
 }
-window.onload=init();
